4eccf94b99c3193075f5a4a64962018dc21886bd
Daria Solomon <99214376+buildasquid@users.noreply.github.com>
2024-10-30 02:03:29 +0100

    Update app.py - app.run corect

--- app.py
+++ app.py
@@ -325,4 +325,4 @@ def payment_feedback():
     return render_template('payment_feedback.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8017, debug=True)
